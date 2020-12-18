9d8b773898965520a1bf215344547fe51343b7b8
elChupaCambra <claudio.cambra@gmail.com>
2020-12-18 20:20:55 +0100

    Added my first, fully custom C program(!!!)

---
+++ times-table.c/times-table.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int times_tabler(int num1, int num2) {
+  char output[15];
+  int count, multiplied;
+  
+  for(count = 1; count <= num2; count++) {
+    multiplied = num1*count;
+    printf("%i x %i = %i\n", num1, count, multiplied);
+  }
+
+  return 0;
+}
+
+int main() {
+  int numToMultiply, timesTableLimit;
+  puts("Input a number.");
+  scanf("%d[^\n]\n", &numToMultiply);
+  puts("Up to which number would you like your times table to go?");
+  scanf("%d[^\n]\n", &timesTableLimit);
+
+  times_tabler(numToMultiply, timesTableLimit);
+
+  return 0;
+}
