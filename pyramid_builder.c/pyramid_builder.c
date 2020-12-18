a0d4d4209ed5f136bf39687fa3bc3f6ece538b4e
elChupaCambra <claudio.cambra@gmail.com>
2020-12-19 00:38:02 +0100

    Fixed artifacting

--- pyramid_builder.c/pyramid_builder.c
+++ pyramid_builder.c/pyramid_builder.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char line[101];
+
 int line_build(int count_num, int height_num) {
-  char line[101];
   int column_count, column_count_pt2;
   for(column_count = 0; column_count <= height_num; column_count++){
     if(column_count > height_num-count_num) {
@@ -29,11 +30,6 @@ int main(int argc, char *argv[]) {
 
   char ch;
   int height = atoi(argv[2]);
-
-  while (height > 19 || height < 1) {
-    printf("%i is not a valid height. Pyramids can be between 1 and 19 lines high.\n");
-    return 2;
-  }
 
   printf("Pyramid with height of %i lines.", height);
 
