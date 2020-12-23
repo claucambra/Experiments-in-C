4c0b01c5c34348a88e4b49d4943c57c0e0544aba
elChupaCambra <claudio.cambra@gmail.com>
2020-12-23 17:19:03 +0100

    Added ascii art for six sided dice

--- dice.c/dice.c
+++ dice.c/dice.c
@@ -15,9 +15,48 @@ void rollDice() {
 	}
 }
 
+
+char dice1[] = "  +--------------------------+\n +----------------------------+\n+-|                           |-+\n|-|                           |-|\n|-|                           |-|\n|-|                           |-|\n|-|           XXXXX           |-|\n|-|          XXXXXXX          |-|\n|-|          XXXXXXX          |-|\n|-|           XXXXX           |-|\n|-|                           |-|\n|-|                           |-|\n|-|                           |-|\n+-|                           |-+\n +-----------------------------+\n  +---------------------------+\n";
+char dice2[] = "  +---------------------------+\n +-----------------------------+\n+-|                           |-+\n|-|   XXXXX                   |-|\n|-|  XXXXXXX                  |-|\n|-|  XXXXXXX                  |-|\n|-|   XXXXX                   |-|\n|-|                           |-|\n|-|                           |-|\n|-|                   XXXXX   |-|\n|-|                  XXXXXXX  |-|\n|-|                  XXXXXXX  |-|\n|-|                   XXXXX   |-|\n+-|                           |-+\n +-----------------------------+\n  +---------------------------+\n";
+char dice3[] = "  +---------------------------+\n +-----------------------------+\n+-|                           |-+\n|-|   XXXXX                   |-|\n|-|  XXXXXXX                  |-|\n|-|  XXXXXXX                  |-|\n|-|   XXXXX   XXXXX           |-|\n|-|          XXXXXXX          |-|\n|-|          XXXXXXX          |-|\n|-|           XXXXX   XXXXX   |-|\n|-|                  XXXXXXX  |-|\n|-|                  XXXXXXX  |-|\n|-|                   XXXXX   |-|\n+-|                           |-+\n +-----------------------------+\n  +---------------------------+\n";
+char dice4[] = "  +---------------------------+\n +-----------------------------+\n+-|                           |-+\n|-|   XXXXX           XXXXX   |-|\n|-|  XXXXXXX         XXXXXXX  |-|\n|-|  XXXXXXX         XXXXXXX  |-|\n|-|   XXXXX           XXXXX   |-|\n|-|                           |-|\n|-|                           |-|\n|-|   XXXXX           XXXXX   |-|\n|-|  XXXXXXX         XXXXXXX  |-|\n|-|  XXXXXXX         XXXXXXX  |-|\n|-|   XXXXX           XXXXX   |-|\n+-|                           |-+\n +-----------------------------+\n  +---------------------------+\n";
+char dice5[] = "  +---------------------------+\n +-----------------------------+\n+-|                           |-+\n|-|   XXXXX           XXXXX   |-|\n|-|  XXXXXXX         XXXXXXX  |-|\n|-|  XXXXXXX         XXXXXXX  |-|\n|-|   XXXXX   XXXXX   XXXXX   |-|\n|-|          XXXXXXX          |-|\n|-|          XXXXXXX          |-|\n|-|   XXXXX   XXXXX   XXXXX   |-|\n|-|  XXXXXXX         XXXXXXX  |-|\n|-|  XXXXXXX         XXXXXXX  |-|\n|-|   XXXXX           XXXXX   |-|\n+-|                           |-+\n +-----------------------------+\n  +---------------------------+\n";
+char dice6[] = "  +---------------------------+\n +-----------------------------+\n+-|                           |-+\n|-|   XXXXX   XXXXX   XXXXX   |-|\n|-|  XXXXXXX XXXXXXX XXXXXXX  |-|\n|-|  XXXXXXX XXXXXXX XXXXXXX  |-|\n|-|   XXXXX   XXXXX   XXXXX   |-|\n|-|                           |-|\n|-|                           |-|\n|-|   XXXXX   XXXXX   XXXXX   |-|\n|-|  XXXXXXX XXXXXXX XXXXXXX  |-|\n|-|  XXXXXXX XXXXXXX XXXXXXX  |-|\n|-|   XXXXX   XXXXX   XXXXX   |-|\n+-|                           |-+\n +-----------------------------+\n  +---------------------------+\n";
+
+
+
 void printDice() {
 	int i = 0;
 	printf("\n");
+	if(sidesDice == 6) {
+		while(diceRack[i] != 0) {
+			switch(diceRack[i]) {
+				case 1:
+					puts(dice1);
+					break;
+				case 2:
+					puts(dice2);
+					break;
+				case 3:
+					puts(dice3);
+					break;
+
+				case 4:
+					puts(dice4);
+					break;
+
+				case 5:
+					puts(dice5);
+					break;
+
+				case 6:
+					puts(dice6);
+					break;
+			}
+			i++;
+		}
+		i = 0;
+	}
 	while(diceRack[i] != 0) {
 		printf("DICE %i: %i\t", i + 1, diceRack[i]); 
 		i++;
