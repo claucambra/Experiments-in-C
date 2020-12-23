f1d2e4b1853a3823351120a1c98980640852d3b4
elChupaCambra <claudio.cambra@gmail.com>
2020-12-23 21:07:08 +0100

    Added statistics

--- dice.c/dice.c
+++ dice.c/dice.c
@@ -3,10 +3,12 @@
 #include <string.h>
 #include <time.h>
 
-int sidesDice = 6;
-int numDice = 2;
+//Dice defaults
+int sides_dice = 6;
+int num_dice = 2;
 
-int diceRack[1000];
+int dice_rack[1000];
+int roll_history[10000];
 
 //ASCII art dice
 const char dice1[] = "  +---------------------------+\n +-----------------------------+\n+-|                           |-+\n|-|                           |-|\n|-|                           |-|\n|-|                           |-|\n|-|           XXXXX           |-|\n|-|          XXXXXXX          |-|\n|-|          XXXXXXX          |-|\n|-|           XXXXX           |-|\n|-|                           |-|\n|-|                           |-|\n|-|                           |-|\n+-|                           |-+\n +-----------------------------+\n  +---------------------------+\n";
@@ -16,21 +18,30 @@ const char dice4[] = "  +---------------------------+\n +-----------------------
 const char dice5[] = "  +---------------------------+\n +-----------------------------+\n+-|                           |-+\n|-|   XXXXX           XXXXX   |-|\n|-|  XXXXXXX         XXXXXXX  |-|\n|-|  XXXXXXX         XXXXXXX  |-|\n|-|   XXXXX   XXXXX   XXXXX   |-|\n|-|          XXXXXXX          |-|\n|-|          XXXXXXX          |-|\n|-|   XXXXX   XXXXX   XXXXX   |-|\n|-|  XXXXXXX         XXXXXXX  |-|\n|-|  XXXXXXX         XXXXXXX  |-|\n|-|   XXXXX           XXXXX   |-|\n+-|                           |-+\n +-----------------------------+\n  +---------------------------+\n";
 const char dice6[] = "  +---------------------------+\n +-----------------------------+\n+-|                           |-+\n|-|   XXXXX   XXXXX   XXXXX   |-|\n|-|  XXXXXXX XXXXXXX XXXXXXX  |-|\n|-|  XXXXXXX XXXXXXX XXXXXXX  |-|\n|-|   XXXXX   XXXXX   XXXXX   |-|\n|-|                           |-|\n|-|                           |-|\n|-|   XXXXX   XXXXX   XXXXX   |-|\n|-|  XXXXXXX XXXXXXX XXXXXXX  |-|\n|-|  XXXXXXX XXXXXXX XXXXXXX  |-|\n|-|   XXXXX   XXXXX   XXXXX   |-|\n+-|                           |-+\n +-----------------------------+\n  +---------------------------+\n";
 
-char commandList[] = "\n\n<---------------------------- COMMANDS ---------------------------->\n\nhelp: prints this cheat sheet\nsetup: choose number of dice and sides of these dice\nroll: roll the dice\nquit: close this program\n\n";
+char commandList[] = "\n\n<---------------------------- COMMANDS ---------------------------->\n\nhelp: prints this cheat sheet\nsetup: choose number of dice and sides of these dice\nstats: show statistics about rolls\nroll (or simply pressing ENTER): roll the dice\nquit: close this program\n\n";
 
-void rollDice() {
-	memset(diceRack, 0, sizeof diceRack);
-	for(int i = 0; i < numDice; i++) {
-		diceRack[i] = rand() % sidesDice + 1;
+void roll_dice() {
+	int roll_total = 0;
+	
+	memset(dice_rack, 0, sizeof dice_rack);
+	for(int i = 0; i < num_dice; i++) {
+		dice_rack[i] = rand() % sides_dice + 1;
+		roll_total += dice_rack[i];
 	}
+	
+	int i = 0;
+	while(roll_history[i] != 0) {
+		i++;
+	}
+	roll_history[i] = roll_total;
 }
 
-void printDice() {
+void print_dice() {
 	int i = 0;
 	printf("\n");
-	if(sidesDice == 6) {
-		while(diceRack[i] != 0) {
-			switch(diceRack[i]) {
+	if(sides_dice == 6) { // Print ASCII pictures for 6-sided dice
+		while(dice_rack[i] != 0) {
+			switch(dice_rack[i]) {
 				case 1:
 					puts(dice1);
 					break;
@@ -40,15 +51,12 @@ void printDice() {
 				case 3:
 					puts(dice3);
 					break;
-
 				case 4:
 					puts(dice4);
 					break;
-
 				case 5:
 					puts(dice5);
 					break;
-
 				case 6:
 					puts(dice6);
 					break;
@@ -57,67 +65,122 @@ void printDice() {
 		}
 		i = 0;
 	}
-	while(diceRack[i] != 0) {
-		printf("DICE %i: %i\t", i + 1, diceRack[i]); 
+	while(dice_rack[i] != 0) {
+		printf("DICE %i: %i\t", i + 1, dice_rack[i]); 
 		i++;
 	}
 	printf("\n");
 }
 
-char charredNumInput[10];
+char charred_num_input[10];
 void setup() {
-	printf("\nHow many sides would you like your dice to have?\n", sidesDice);
-	fgets(charredNumInput, 9, stdin);
-	if(atoi(charredNumInput))
-		sidesDice = atoi(charredNumInput);
+	printf("\nHow many sides would you like your dice to have?\n", sides_dice);
+	fgets(charred_num_input, 9, stdin);
+	if(atoi(charred_num_input))
+		sides_dice = atoi(charred_num_input);
 	else
 		puts("No number detected.");
-	memset(charredNumInput, 0, sizeof charredNumInput);
-
-	printf("\nHow many dice would you like to start with?\n", numDice);
+	memset(charred_num_input, 0, sizeof charred_num_input);
+	
+	printf("\nHow many dice would you like to start with?\n", num_dice);
 	do {
-		if(numDice > 999)
+		if(num_dice > 999)
 			puts("Max number of dice is 999");
 		
-		fgets(charredNumInput, 9, stdin);
-		if(atoi(charredNumInput))
-			numDice = atoi(charredNumInput);
+		fgets(charred_num_input, 9, stdin);
+		if(atoi(charred_num_input))
+			num_dice = atoi(charred_num_input);
 		else
 			puts("No number detected.");
-	} while (numDice > 999);
-	memset(charredNumInput, 0, sizeof charredNumInput);
+	} while (num_dice > 999);
+	memset(charred_num_input, 0, sizeof charred_num_input);
 	
-	printf("NEW SETTINGS: Dice sides: %i, Dice amount: %i\n\n", sidesDice, numDice);
+	printf("NEW SETTINGS: Dice sides: %i, Dice amount: %i\n\n", sides_dice, num_dice);
+	
+	roll_dice();
+}
 
-	rollDice();
+void show_stats() {
+	if (roll_history[0] == 0) 
+		puts("\nNo stats available yet. Roll the dice!\n");
+	else {
+		int min_num = roll_history[0], max_num = roll_history[0], nextnum, i = 0;
+		while(roll_history[i] != 0) { 
+			nextnum = roll_history[i];
+			
+			if(min_num > nextnum)
+				min_num = nextnum;
+			
+			i++;
+		}
+		i = 0;
+		
+		while(roll_history[i] != 0) { 
+			nextnum = roll_history[i];
+			
+			if(max_num < nextnum)
+				max_num = nextnum;
+			
+			i++;
+		}
+		i = 0;
+		
+		//Loop prints bar chart
+		puts("\nFREQUENCY OF TOTALS:");
+		int current_total = 0, this_num_count = 0;
+		char bar[200];
+		for(int num = min_num; num <= max_num; num++) {
+			
+			while(roll_history[current_total] != 0) { //Count number of occurrences of each total
+				if (roll_history[current_total] == num)
+					this_num_count++;
+				current_total++;
+			}
+			
+			int barcount;
+			for(barcount = 0; barcount < this_num_count; barcount++) { //Draw bar for bar chart
+				bar[barcount] = '#';
+			}
+			bar[barcount] = '\0'; //Prevent garbled string being read
+			
+			printf("%i:\t%s\n", num, bar);
+			
+			memset(bar, 0, sizeof bar);
+			current_total = 0;
+			this_num_count = 0;
+		}
+	}
 }
 
 int main() {
 	srand(time(NULL)); //Random num generator for dice, seed set as current time
-
+	
 	puts(commandList);
-	printf("DEFAULTS: Dice sides: %i, Dice amount: %i\n\n", sidesDice, numDice);
-
+	printf("DEFAULTS: Dice sides: %i, Dice amount: %i\n\n", sides_dice, num_dice);
+	
 	char input[10];
 	do {
 		memset(input, 0, sizeof input);
 		fgets(input, 9, stdin);
 		input[strcspn(input, "\n")] = 0;
-
+		
 		if(strcmp(input, "quit") == 0)
 			break;
 		else if (strcmp(input, "help") == 0)
 			puts(commandList);
 		else if (strcmp(input, "setup") == 0)
 			setup();
+		else if (strcmp(input, "stats") == 0)
+			show_stats();
 		else if (strcmp(input, "roll") == 0) {
-			rollDice(); 
-			printDice();
+			roll_dice(); 
+			print_dice();
 		} else {
-			puts("No command detected"); 
+			roll_dice(); 
+			print_dice(); 
 			continue;
 		}
 	} while (strcmp(input, "quit") != 0);
-
+	
 	return 0;
 }
