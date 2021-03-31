6837e6ff864d57ccb0642c9807a10d594cf7aa34
Claudio Cambra <claudio.cambra@gmail.com>
2021-03-31 20:00:59 +0200

    Added a simple implementation of quick sort

---
+++ quick-sort.c/quick-sort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int swap(int *num_a, int *num_b) {
+	int holder = *num_a;
+	*num_a = *num_b;
+	*num_b = holder;
+}
+
+/* 1. Takes last element as pivot
+ * 2. Places pivot element at its correct position in sorted array
+ * 3. Places all elements smaller than pivot element to the left of pivot element
+ * 4. Places all elements larger than pivot element to the right of pivot element
+ */
+int partition(int array[], int low, int high) {
+	int pivot = array[high];
+	
+	int i = (low - 1);
+	
+	for (int j = low; j <= high - 1; j++) {
+		if (array[j] < pivot) {
+			i++;
+			swap(&array[i], &array[j]);
+		}
+	}
+	swap(&array[i + 1], &array[high]);
+	return(i + 1);
+}
+
+// Quick sort picks an element as pivot and partitions around this pivot.
+// It recursively calls itself
+void quick_sort(int array[], int low, int high) {
+	if (low < high) {
+		int partition_index = partition(array, low, high);
+		quick_sort(array, low, partition_index - 1);
+		quick_sort(array, partition_index + 1, high);
+	}
+}
+
+int main () {
+	int num_array[50], array_length, i;
+	puts("How long would you like your array to be?");
+	scanf("%d", &array_length);
+	
+	puts("Enter your numbers.");
+	for(i = 0; i < array_length; i++)
+		scanf("%d", &num_array[i]);
+	quick_sort(num_array, 0, array_length);
+	
+	puts("Sorted array:");
+	for(i = 0; i < array_length; i++)
+		printf("%d\t", num_array[i]);
+	puts("Done!");
+	return 0;
+}
